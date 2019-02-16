57d9fe45298674da49394c2d5d7a0c0edd4c87c7
anonym <asd@cvb.org>
2019-02-16 19:53:07 +0200

    update

--- load_pics.js
+++ load_pics.js
@@ -16,7 +16,7 @@ xhr.onload = (stat) => {
 		console.error(stat)
 		return
 	}
-	$('#img').src = new Blob(new Uint8Array(xhr.response), {type: 'image/png'})
+	$('#img').src = new Blob(new Uint8Array(xhr.response), {type: 'image/jpeg'})
 }
 
 xhr.send()
