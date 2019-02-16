14276d93f44f8214d01e97e6d07f12c7a959c892
anonym <asd@cvb.org>
2019-02-16 19:42:12 +0200

    update

--- load_pics.js
+++ load_pics.js
@@ -8,6 +8,8 @@ xhr.responsetype='arraybuffer'
 xhr.open('GET', "https://homework-63c7.restdb.io/media/" + type)
 
 xhr.setRequestHeader('x-apikey','5c67dd3bad19dc08b020d499')
+xhr.setRequestHeader('content-type','image/jpeg')
+xhr.setRequestHeader('cache-control','no-cache')
 
 xhr.onload = (stat) => {
 	if (!xhr.response) {
