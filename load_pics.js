13670a91ddf20ab4ea7011455cb22d8f4f1bc958
anonym <asd@cvb.org>
2019-02-16 12:05:39 +0200

    update

--- load_pics.js
+++ load_pics.js
@@ -1,17 +1,2 @@
 type = (new URL(location.href)).searchParams.get("t")
-
-on_count = (type) => {
-    return (xhr, stat) => {
-        if (stat != 'success') {
-            console.error('on_count ' + stat)
-            return
-        }
-        n = parseInt(xhr.responseText)
-        for (i = 1; i <= n; i++) {
-            if (id < 10) id = '0' + id
-            $('#layout').append('<img src="/hw/' + type + '/0' + id + '.png"></img>')
-        }
-    }
-}
-
-$.ajax('/hw/' + type + '/imgc', on_count(type))
+$("#img").src = "https://homework-63c7.restdb.io/media/"+type
