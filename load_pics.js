c89ebf48d6f54e536be2591ce02cc59fe1febb62
anonym <asd@cvb.org>
2019-02-16 19:20:16 +0200

    update

--- load_pics.js
+++ load_pics.js
@@ -7,13 +7,14 @@ xhr.responsetype='blob'
 
 xhr.open('GET', "https://homework-63c7.restdb.io/media/" + type)
 
+xhr.setRequestHeader('x-apikey','5c67dd3bad19dc08b020d499')
+
 xhr.onload = (stat) => {
 	if (!xhr.response) {
 		console.error(stat)
 		return
 	}
 	$('#img').src = new Blob([xhr.response], {type: 'image/png'})
-	alert('OK')
 }
 
 xhr.send()
