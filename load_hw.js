8c5ad6cd7fc1754141c6474d7a758106832a714c
anonym <asd@cvb.org>
2019-02-18 20:36:42 +0200

    update

--- load_hw.js
+++ load_hw.js
@@ -1,29 +1,25 @@
-layout = $('#layout')
-url = "https://homework-63c7.restdb.io/rest/email_inbound"
-$.ajax({
-	url: url,
-	beforeSend: (xhr) => {
-		xhr.setRequestHeader('x-apikey', '5c67dd3bad19dc08b020d499')
-	},
-	complete: (xhr, stat) => {
-		if (stat != 'success') {
-			console.error(stat)
-			return
-		}
-		window.predmets = JSON.parse(xhr.responseText)
-		$.ajax('/card.tmp', {
-			complete: (xhr, stat) => {
-				if (stat != 'success') {
-					console.error(stat)
-					return
-				}
-				window.template = xhr.responseText
-				for (var i = window.predmets.length - 1; i >= 0; i--) {
-					predmet = window.predmets[i];
-					card = template.replace('TITLE', predmet.subject).replace('TEXT', predmet.body)
-					layout.append(card)
-				}
-			}
-		})
-	}
+layout = document.getElementById('layout')
+url = 'https://homework-63c7.restdb.io/rest/email_inbound'
+fetch(url, {
+    headers: {
+        'x-apikey': '5c67dd3bad19dc08b020d499'
+    }
+}).then(res => {
+    if (!res.ok) {
+        console.error(res)
+        return
+    }
+    window.res = res.json()
+    fetch('/card.tmp').then(res => {
+        if(!res.ok) {
+            console.error(res)
+            return
+        }
+        window.tmp = res.text()
+        for(var i = window.res.length - 1; i >= 0; i--) {
+            pred = window.res[i]
+            card = window.tmp.replace('TITLE', pred.subject).replace('TEXT', pred.body)
+            layout.insertAdjacentHTML('beforeend', card)
+        }
+    })
 })
