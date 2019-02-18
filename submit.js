d7f2410da7899e05e7f699760fb2e708d94a1a7a
anonym <asd@cvb.org>
2019-02-18 20:25:59 +0200

    update

--- submit.js
+++ submit.js
@@ -10,8 +10,12 @@ document.getElementById('imgfile').onchange = () =>
 	fd.append('subject', document.getElementById('name').value)
 	fd.append('body', document.getElementById('desc').value)
 	fetch('https://homework-63c7.restdb.io/rest/email_inbound', {
-		method: 'PUT',
+		method: 'POST',
 		body: fd,
-		headers: { 'x-apikey': '5c67dd3bad19dc08b020d499' }
+		headers: {
+                    'cache-control': 'no-cache',
+                    'x-apikey': '5c67dd3bad19dc08b020d499',
+                    'content-type': 'application/json'
+                }
 	}).then(response => response.ok ? console.log('POST OK') : console.error('POST not OK'))
 }
