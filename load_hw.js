df1a570d2a5c2ec7abdc37e0ee39366553f60fce
anonym <asd@cvb.org>
2019-02-18 20:59:15 +0200

    update

--- load_hw.js
+++ load_hw.js
@@ -4,22 +4,15 @@ fetch(url, {
     headers: {
         'x-apikey': '5c67dd3bad19dc08b020d499'
     }
-}).then(res => {
-    if (!res.ok) {
-        console.error(res)
-        return
-    }
-    window.res = res.json()
-    fetch('/card.tmp').then(res => {
-        if(!res.ok) {
-            console.error(res)
-            return
-        }
-        window.tmp = res.text()
-        for(var i = window.res.length - 1; i >= 0; i--) {
-            pred = window.res[i]
-            card = window.tmp.replace('TITLE', pred.subject).replace('TEXT', pred.body)
+}).then(res => { res.ok ? res.json() : console.error(res)}).then(res => {
+    window.preds = res
+    fetch('/card.tmp').then(res => res.ok ? res.text() : console.error(res)).then(template => {
+        window.temp = template
+        for (var i = 0; i < window.preds.length; i++) {
+            pred = preds[i]
+            card = window.temp.replace('TITLE', pred['subject']).replace('TEXT', pred['body'])
             layout.insertAdjacentHTML('beforeend', card)
         }
     })
 })
+
