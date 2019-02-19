883af7d878fa3e5d108c57424baff4173803bc01
anonym <asd@cvb.org>
2019-02-19 20:49:48 +0200

    Useful text

--- load_hw.js
+++ load_hw.js
@@ -1,18 +1,30 @@
 layout = document.getElementById('layout')
+group = document.getElementById('group')
 url = 'https://homework-63c7.restdb.io/rest/email_inbound'
-fetch(url, {
+
+function update() {
+  fetch(url, {
     headers: {
-        'x-apikey': '5c67dd3bad19dc08b020d499'
+      'x-apikey': '5c67dd3bad19dc08b020d499'
     }
-}).then(res => res.json()).then(res => {
+  }).then(res => res.json()).then(res => {
     window.preds = res
     fetch('/card.tmp').then(res => res.text()).then(template => {
-        window.temp = template
-        for (var i = 0; i < window.preds.length; i++) {
-            pred = preds[i]
-            card = window.temp.replace('TITLE', pred['subject']).replace('TEXT', pred['body'])
-            layout.insertAdjacentHTML('beforeend', card)
-        }
+      window.temp = template
+      layout.innerHTML = ''
+      for (var i = 0; i < window.preds.length; i++) {
+        pred = preds[i]
+        if (pred.subject.slice(-1) != (group.checked?'1':'2')) continue;
+        card = window.temp.replace('TITLE', pred['subject']).replace('TEXT', pred['body'])
+        layout.innerHTML += card
+      }
     })
-})
+  })
+}
+update()
 
+group.onclick = () => {
+  group.value = (group.checked?'1':'2')+' группа'
+  document.querySelector('.mdl-js-snackbar').MaterialSnackbar.showSnackbar({message: 'Загрузка...'})
+  update()
+}
