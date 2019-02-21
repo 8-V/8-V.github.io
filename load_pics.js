d6f0cd4c5cb16740a8c4200005c636d3004bb251
8-V <45600990+8-V@users.noreply.github.com>
2019-02-21 18:23:44 +0200

    update api key

--- load_pics.js
+++ load_pics.js
@@ -2,6 +2,6 @@ type = (new URL(location.href)).searchParams.get("t")
 img = document.getElementById('img')
 
 fetch('https://homework-63c7.restdb.io/media/' + type, {
-    headers: { 'x-apikey': '5c67dd3bad19dc08b020d499', 'cache-control': 'no-cache' },
+    headers: { 'x-apikey': '5c6ecf1828ca2e129e8696e8', 'cache-control': 'no-cache' },
     method: 'GET'
 }).then(res => res.ok ? img.src = res.blob() : console.error(res))
