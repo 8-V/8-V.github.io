d19016484b92f7d7dd48d10f8cf7363d21b604d3
megdimka <megdimka@vivaldi.net>
2019-03-16 15:25:11 +0200

    gm

--- README.md
+++
@@ -1,5 +0,0 @@
-# 8v
-[![Netlify Status](https://api.netlify.com/api/v1/badges/4ee7dc2f-d4b3-43f2-8268-54d75d251ca9/deploy-status)](https://app.netlify.com/sites/confident-swirles-e05db0/deploys)  
-[8-B website](http://8v-dz.tk)  
-[Github](https://github.com/8-v/8v)  
-[License](https://github.com/8-v/8v/blob/master/LICENSE)  
--- load_pics.js
+++
@@ -1,7 +0,0 @@
-type = (new URL(location.href)).searchParams.get("t")
-img = document.getElementById('img')
-
-fetch('https://homework-63c7.restdb.io/media/' + type, {
-    headers: { 'x-apikey': '5c6ecf1828ca2e129e8696e8', 'cache-control': 'no-cache' },
-    method: 'GET'
-}).then(res => res.ok ? img.src = res.blob() : console.error(res))
