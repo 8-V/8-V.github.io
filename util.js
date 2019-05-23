3ce0c7dc2448ac84bd0d538cdec8054c08058128
teki <teki@asd.f>
2019-05-23 22:10:38 +0300

    added screamer

--- util.js
+++ util.js
@@ -1,4 +1,20 @@
-function sqr(x)
-{
-    return x*x;
+function sqr(x) {
+  return x * x;
 }
+
+window.onload = _ => {
+  btn = [...document.all].filter(x => x.nodeName == 'BUTTON')[0];
+};
+
+cheats = _ => {
+  document.querySelector('.cheat').classList.add('fullscreen');
+  [...document.all].map(x => {
+    x.animate(
+      [{background: 'white'}, {background: 'black'}, {background: 'white'}],
+      {
+        duration: 1000,
+        iterations: Infinity,
+      },
+    );
+  });
+};
